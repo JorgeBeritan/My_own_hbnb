319a3591348fabf884c466bf7c59db380424c698
NicoBoni55 <bonifrank55@gmail.com>
2024-07-07 23:22:49 -0300

    table diagram && scripts

--- hbnb/models/amenity.py
+++ hbnb/models/amenity.py
@@ -24,3 +24,31 @@ class Amenity(db.Model):
             "id": self.id,
             "name": self.name
             }
+        
+class PlaceAmenites(db.Model):
+
+    __tablename__ = 'place amenities'
+
+    id = db.Column(db.String(36) primary_key=True, nullable= False)
+    place_id = db.Column(db.String(36) nullable=False)
+    amenity_id = db.Column(db.String(36) nullable=False)
+
+    def __init__(
+        self.id = id
+        self.place_id = place_id
+        self.amenity_id = amenity_id
+    ):
+        self.id = str(uuid.uuid4())
+        self.place_id = str(uuid.uuid4())
+        self.amenity_id = str(uuid.uuid4())
+    
+    def __repr__(self):
+        return f"<PlaceAmenity ({self.place_id} - {self.amenity_id})>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "place_id" self.place_id,
+            "amenity_id" self.amenity_id
+        }
+    
